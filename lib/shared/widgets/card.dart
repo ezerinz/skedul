987aff21b9c4eee880d9c9e851c9bc7c5c5100fc
Edwin <edwinnnzx@gmail.com>
2024-08-25 13:09:40 +0800

    fix: card color bug

--- lib/shared/widgets/card.dart
+++ lib/shared/widgets/card.dart
@@ -34,121 +34,111 @@ class CustomCard extends ConsumerWidget {
     final List<double> stops = [0.0, fillStop, fillStop, 1.0];
     final isDark = ref.watch(isDarkModeProvider);
 
-    return Container(
-      width: double.infinity,
-      height: 81,
-      padding: const EdgeInsets.only(left: 10.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: Container(
+        width: double.infinity,
+        height: 81,
         color: backgroundColor.withOpacity(isDark ? 0.15 : 0.30),
-        // gradient: LinearGradient(
-        //   colors: [
-        //     foregroundColor.withOpacity(isDark ? 0.50 : 0.8),
-        //     foregroundColor.withOpacity(isDark ? 0.50 : 0.8),
-        //     backgroundColor.withOpacity(isDark ? 0.15 : 0.30),
-        //     backgroundColor.withOpacity(isDark ? 0.15 : 0.30)
-        //   ],
-        //   stops: stops,
-        // ),
-      ),
-      child: Center(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 15,
-              color: foregroundColor.withOpacity(
-                isDark ? 0.50 : 0.8,
-              ),
-            ),
-            Expanded(
-              child: InkWell(
-                onTap: onCardTap,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (titleWidget != null) titleWidget!,
-                      if (titleWidget == null)
-                        Text(
-                          title,
-                          style: const TextStyle(
-                              // color: foregroundColor,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.meeting_room,
-                            size: 14,
-                            // color: foregroundColor.withOpacity(0.75),
-                          ),
-                          const SizedBox(
-                            width: 6.0,
-                          ),
-                          Expanded(
-                            child: Text(
-                              subtitle1,
-                              style: AppTheme.kTextSemiBold24.copyWith(
-                                fontSize: 14,
-                                // color: foregroundColor.withOpacity(0.75),
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.access_time,
-                            size: 14,
-                            // color: foregroundColor.withOpacity(0.75),
-                          ),
-                          const SizedBox(
-                            width: 6.0,
-                          ),
-                          Expanded(
-                            child: Text(
-                              subtitle2,
-                              style: AppTheme.kTextSemiBold24.copyWith(
-                                fontSize: 14,
-                                // color: foregroundColor.withOpacity(0.75),
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+        child: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 8,
+                color: foregroundColor.withOpacity(
+                  isDark ? 0.50 : 0.8,
                 ),
               ),
-            ),
-            if (actionIcon != null)
-              SizedBox(
-                width: 50,
+              Expanded(
                 child: InkWell(
-                  borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(10.0),
-                      bottomRight: Radius.circular(10.0)),
-                  onTap: actionPressed,
-                  child: Center(
-                    child: Icon(
-                      actionIcon!,
-                      color: backgroundColor,
-                      size: 25.0,
+                  onTap: onCardTap,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (titleWidget != null) titleWidget!,
+                        if (titleWidget == null)
+                          Text(
+                            title,
+                            style: const TextStyle(
+                                // color: foregroundColor,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.meeting_room,
+                              size: 14,
+                              // color: foregroundColor.withOpacity(0.75),
+                            ),
+                            const SizedBox(
+                              width: 6.0,
+                            ),
+                            Expanded(
+                              child: Text(
+                                subtitle1,
+                                style: AppTheme.kTextSemiBold24.copyWith(
+                                  fontSize: 14,
+                                  // color: foregroundColor.withOpacity(0.75),
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.access_time,
+                              size: 14,
+                              // color: foregroundColor.withOpacity(0.75),
+                            ),
+                            const SizedBox(
+                              width: 6.0,
+                            ),
+                            Expanded(
+                              child: Text(
+                                subtitle2,
+                                style: AppTheme.kTextSemiBold24.copyWith(
+                                  fontSize: 14,
+                                  // color: foregroundColor.withOpacity(0.75),
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
-          ],
+              if (actionIcon != null)
+                SizedBox(
+                  width: 50,
+                  child: InkWell(
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0)),
+                    onTap: actionPressed,
+                    child: Center(
+                      child: Icon(
+                        actionIcon!,
+                        color: backgroundColor,
+                        size: 25.0,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
